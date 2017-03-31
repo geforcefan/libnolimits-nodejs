f91dbf973dd0430a2bcf49e54d58c7ec832ed922
Ercan Akyürek <ercan.akyuerek@gmail.com>
2017-03-31 10:40:57 +0200

    added dev dep
    added dev dep

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-var SegfaultHandler = require('segfault-handler');
-
-SegfaultHandler.registerHandler("crash.log"); // With no argument, SegfaultHandler will generate a generic log file name
-
-// Optionally specify a callback function for custom logging. This feature is currently only supported for Node.js >= v0.12 running on Linux.
-SegfaultHandler.registerHandler("crash.log", function(signal, address, stack) {
-    // Do what you want with the signal, address, or stack (array)
-    // This callback will execute before the signal is forwarded on.
-});
-
-
 const NoLimits = require('./build/Release/nolimits');
-const Park = new NoLimits.NL2.Park("/Users/ercanakyurek/Desktop/newpark/parks/Hydra/Hydra.nl2park");
-
-console.log(Park);
-console.log(NoLimits.NL2.Weather.convertFogIntensity(3, true), NoLimits.NL2.Weather.convertWindIntensity(3, true));
+module.exports.default = NoLimits;
